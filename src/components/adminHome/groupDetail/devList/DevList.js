5b0ba9253257d39de7ce1a061779ccfcdbca8f52
Tess Lameyer <tjlameyer@gmail.com>
2020-02-20 16:46:08 -0800

    Tess JBJ hardcoded data for devlist

--- src/components/adminHome/groupDetail/devList/DevList.js
+++ src/components/adminHome/groupDetail/devList/DevList.js
@@ -4,7 +4,20 @@ import Dev from './dev/Dev';
 const DevList = () => {
 
   const array = [
-
+    {
+      imageURL: 'https://placebear.com/100/100',
+      timestamp: '9249359', 
+      displayName: 'dev display name 1',
+      repoName: 'repo1', 
+      commitMessage: 'hello this is my commit'
+    }, 
+    {
+      imageURL: 'https://placebear.com/101/100',
+      timestamp: '92493654', 
+      displayName: 'dev display name 2',
+      repoName: 'repo2', 
+      commitMessage: 'hello this is my commit2'
+    }
   ];
 
   const listOfDevElements = array.map(dev => (
@@ -13,13 +26,11 @@ const DevList = () => {
     </li>
   ));
 
-
   return (
     <ul>
       {listOfDevElements}
     </ul>
   );
-
 };
 
 export default DevList;
--- src/components/adminHome/groupDetail/devList/dev/Dev.js
+++ src/components/adminHome/groupDetail/devList/dev/Dev.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Dev = () => {
+const Dev = ({ imageURL, timestamp, displayName, commitMessage, repoName }) => {
 
   return (
     <>
-      {/* <img src={image} /> */}
-      <h4>displayName</h4>
-      <p>repo name</p>
-      <p>commit message</p>
-      <p>timestamp</p>
+      <img src={imageURL} />
+      <h4>{displayName}</h4>
+      <p>{repoName}</p>
+      <p>{commitMessage}</p>
+      <p>{timestamp}</p>
     </>
-
   );
+};
 
+Dev.propTypes = {
+  imageURL: PropTypes.string.isRequired,
+  timestamp: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  commitMessage: PropTypes.string.isRequired,
+  repoName: PropTypes.string.isRequired
 };
 
 export default Dev;
