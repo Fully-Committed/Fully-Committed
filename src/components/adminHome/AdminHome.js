8e208209824c6221b6f799a78325192dc8c0d578
Jodi 'JBJ' Kansagor <51837529+jodinkansagor@users.noreply.github.com>
2020-02-20 17:26:07 -0800

    Front end component structure (#2)
    * Tess and JBJ boiler plate config and scaffolding
    
    * Tess and JBJ Header component draft
    
    * Tess and JBJ Loading component drafted
    
    * Tess JBJ Dev and DevList drafts
    
    * Tess JBJ hardcoded data for devlist
    
    * Tess JBJ Group and GroupList drafted
    
    * Tess JBJ AdminHome drafted
    
    * Tess JBJ scaffolding for AdminGroupForm page
    
    Co-authored-by: Tess <52361640+tess-jl@users.noreply.github.com>

---
+++ src/components/adminHome/AdminHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GroupList from './groupList/GroupList';
+import GroupDetail from './groupDetail/GroupDetail';
+
+const AdminHome = () => (
+  <>
+    <GroupList />
+    <GroupDetail />
+  </>
+);
+
+export default AdminHome;
