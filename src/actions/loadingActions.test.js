c6b55de828cccf1ce2e4f430c99aba746b072a18
joelpdurham <joeldur@gmail.com>
2020-02-24 15:33:30 -0800

    fixed app snapshot

--- src/actions/loadingActions.test.js
+++ src/actions/loadingActions.test.js
@@ -4,11 +4,11 @@ import { turnLoadingOn, turnLoadingOff } from './loadingActions';
 describe('Loading Actions', () => {
   it('call loading on', () => {
     const action = turnLoadingOn();
-    expect(action).toEqual('LOADING_ON');
+    expect(action.type).toEqual('LOADING_ON');
   });
 
   it('can call loading off', () => {
     const action = turnLoadingOff();
-    expect(action).toEqual('LOADING_OFF');
+    expect(action.type).toEqual('LOADING_OFF');
   });
 });
