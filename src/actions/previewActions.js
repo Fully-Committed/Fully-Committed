f48ee66ad322b4c5137178311b968ee3090a4871
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-25 11:28:54 -0800

    added previewActions.js

---
+++ src/actions/previewActions.js
@@ -0,0 +1,10 @@
+import { turnLoadingOn, turnLoadingOff } from './loadingActions';
+
+export const SET_PREVIEW_GROUP_NAME = 'SET_PREVIEW_GROUP_NAME';
+
+export const ADD_DEV = 'ADD_DEV';
+
+export const REMOVE_DEV = 'REMOVE_DEV';
+
+
+
