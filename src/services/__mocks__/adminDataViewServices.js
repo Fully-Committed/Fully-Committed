a44abfde334449887a93fe7d6fd52f3c19f28956
Jodi N Kansagor <joedvice@gmail.com>
2020-02-25 13:53:23 -0800

    added group array action

---
+++ src/services/__mocks__/adminDataViewServices.js
@@ -0,0 +1,13 @@
+export const getGroups = (adminId) => {
+  return Promise.resolve([{
+    admindIds: ['12', '34', adminId],
+    devsInGroups: [{
+      devName: 'dev1',
+      devGitHubHandle: 'gitbud'
+    }, {
+      devName: 'dev2',
+      devGitHubHandle: 'gitfriend'
+    }],
+    groupName: 'My group'
+  }]);
+};
