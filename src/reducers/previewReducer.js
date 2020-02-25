d143ccf88960c299531888bef63c487a39e63968
Tess Lameyer <tjlameyer@gmail.com>
2020-02-25 13:39:33 -0800

    test passing for addDev case

--- src/reducers/previewReducer.js
+++ src/reducers/previewReducer.js
@@ -14,6 +14,8 @@ export default function reducer(state = initialState, action){
   switch(action.type) {
     case SET_PREVIEW_GROUP_NAME:
       return { ...state, previewName: action.payload };
+    case ADD_DEV: 
+      return { ...state, previewDev: action.payload };
     default: 
       return state; 
   }
