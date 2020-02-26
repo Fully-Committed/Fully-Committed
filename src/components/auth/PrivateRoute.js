bd0716fc3b2246ed8a6ee48817885e403f8f0659
Jodi N Kansagor <joedvice@gmail.com>
2020-02-26 11:34:03 -0800

    commit to pull dev

---
+++ src/components/auth/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ component, patth}) => ();
+
+const loading = useSelector(getL)
+
+PrivateRoute.propTypes = {};
+
+export default PrivateRoute;
