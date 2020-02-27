14b2128acb82fc9546943e8a4ad761f1ffc098ed
Tess <52361640+tess-jl@users.noreply.github.com>
2020-02-27 15:44:14 -0800

    Card 26 change token (#23)
    * changed localhost to mongodb uri
    
    * updated API URL
    
    * changed how token is attached
    
    Co-authored-by: Joel Patrick Durham <joeldur@gmail.com>

--- src/components/adminDataView/AdminDataView.js
+++ src/components/adminDataView/AdminDataView.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GroupList } from './GroupList';
 import { DataView } from './DataView';
-import styles from './AdminDataView.css'
+import styles from './AdminDataView.css';
+import { useLocation } from 'react-router-dom';
+import { setAccesToken } from '../../services/adminDataViewServices';
+
 
 export const AdminDataView = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const token = params.get('token');
+    if(token) setAccesToken(token);
+  }, []);
   
   return (
     <section className={styles.admindata}>
