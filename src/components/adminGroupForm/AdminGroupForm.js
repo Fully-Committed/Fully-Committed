620e68bc071c21990b602e75be573c4b626879f6
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-26 17:04:44 -0800

    working on group POST route, switching drivers

--- src/components/adminGroupForm/AdminGroupForm.js
+++ src/components/adminGroupForm/AdminGroupForm.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { GroupNameForm } from './groupNameForm/GroupNameForm';
 import { AddDevForm } from './addDev/AddDev';
 import { GroupPreview } from './groupPreview/GroupPreview';
+import { useSelector } from 'react-redux';
+import { toGetPreviewDevs } from '../../selectors/useSelectors';
+import { usePreview } from '../../hooks/usePreview';
 
 const AdminGroupForm = () => {
+  const devsInGroupArray = useSelector(toGetPreviewDevs);
+  const { groupName, groupDescription } = usePreview(); 
+
+  const groupToPost = { 
+    groupName: groupName,
+    groupDescription: groupDescription, 
+    devsInGroup: devsInGroupArray, 
+    adminIds: ['adminID 1']
+  };
+
+
+
 
   const postGroup = () => {
     ///dispact action to POST a new group ////
