6984223e7749485782fd7b0a30d003a472d8a893
joelpdurham <joeldur@gmail.com>
2020-02-26 18:30:05 -0800

    added missing semi colon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { toGetUserSession } from '../../selectors/useSelectors' 
+import { toGetUserSession } from '../../selectors/useSelectors';
 import { useLogout } from '../../hooks/useLogout';
 
 const Header = () => {
