2ce6e0fabf6f2883d3e9a393749b1d0dd6040585
joelpdurham <joeldur@gmail.com>
2020-02-27 17:39:18 -0800

    slight clean up

--- src/components/adminDataView/AdminDataView.js
+++ src/components/adminDataView/AdminDataView.js
@@ -2,24 +2,10 @@ import React from 'react';
 import { GroupList } from './GroupList';
 import { DataView } from './DataView';
 import styles from './AdminDataView.css';
-// import { useLocation } from 'react-router-dom';
 
-export const AdminDataView = () => {
-  // const location = useLocation();
-
-  // useEffect(() => {
-  //   const params = new URLSearchParams(location.search);
-  //   const token = params.get('token');
-  //   if(token) {
-  //     localStorage.setItem('token', token);
-  //     window.location.href = './';
-  //   }
-  // }, []);
-  
-  return (
-    <section className={styles.admindata}>
-      <GroupList />
-      <DataView />
-    </section>
-  );
-};
+export const AdminDataView = () => (
+  <section className={styles.admindata}>
+    <GroupList />
+    <DataView />
+  </section>
+);
