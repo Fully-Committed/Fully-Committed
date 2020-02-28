b833c5a9b43a2613c7e9e8cbd1aaf5e263f023fa
Jodi N Kansagor <joedvice@gmail.com>
2020-02-27 18:23:16 -0800

    maybe now

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
 
   return (
     <header>
-      <h1>Commit Ki77y</h1>
+      <h1>Commit Kitty</h1>
       <img className={styles.headerImage} src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/142_Github_logo_logos-512.png' />
       <FormButton />
       {logoutButton}
--- src/reducers/groupsReducer.js
+++ src/reducers/groupsReducer.js
@@ -9,7 +9,7 @@ const initialState = {
 export const groupsReducer = (state = initialState, action) => {
   switch(action.type) {
     case CREATE_GROUP:
-      return { ...state, group: [...state.group, action.payload] };
+      return { ...state, groups: [...state.groups, action.payload] };
     case SET_GROUP_SESSION:
       return { ...state, groups: action.payload };
     case SET_GROUP_ERROR:
