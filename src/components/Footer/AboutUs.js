6eac0f5da66bee5902e3d9f424d1c6c5d8f7242f
Jodi 'JBJ' Kansagor <51837529+jodinkansagor@users.noreply.github.com>
2020-02-27 16:02:02 -0800

    Card 27 about us (#24)
    * fixed date sort function
    
    * style logout
    
    * group name, add dev styling
    
    * working on form
    
    * styling form still
    
    * styling mostly finished
    
    * mobile styling for login
    
    * login styling
    
    * add about us button and page
    
    * more styling of about us
    
    * header for about page
    
    * change title

---
+++ src/components/Footer/AboutUs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './AboutUs.css';
+
+export const AboutUs = () => (
+
+  <section className={styles.wholeAbout}>
+    <h1>Meet the Commit Kitty Developers</h1>
+    <section className={styles.aboutus}>
+      <section className={styles.dev}>
+        <img src="https://avatars3.githubusercontent.com/u/53454869?s=460&v=4" />
+        <h1>Aaron Edward  Glenn</h1>
+        <a href="https://github.com/aaronedwardglenn">Github</a>
+      </section>
+      <section className={styles.dev}>
+        <img src="https://avatars0.githubusercontent.com/u/51837529?s=460&v=4" />
+        <h1>Jodi &apos;JBJ&apos; Kansagor</h1>
+        <a href="https://github.com/jodinkansagor">Github</a>
+      </section>
+      <section className={styles.dev}>
+        <img src="https://avatars2.githubusercontent.com/u/54227387?s=460&v=4" />
+        <h1>Joel Patrick Durham</h1>
+        <a href="https://github.com/joelpdurham">Github</a>
+      </section>
+      <section className={styles.dev}>
+        <img src="https://avatars3.githubusercontent.com/u/52361640?s=460&v=4" />
+        <h1>Tess Lameyer</h1>
+        <a href="https://github.com/tess-jl">Github</a>
+      </section>
+    </section>
+  </section>
+);
